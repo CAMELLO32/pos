5132ce8054ce1dde292275f00b8a609e2855c26f
Jhon Alexander Miranda <camello32@gmail.com>
2018-04-08 13:25:22 -0500

    3 maquetación del modulo de Usuarios
    se agrega el modal para agregar usuarios

--- vistas/modulos/usuarios.php
+++ vistas/modulos/usuarios.php
@@ -1,52 +1,231 @@
 
   <div class="content-wrapper">
 
-    <section class="content-header">
+  <section class="content-header">
 
-      <h1>
+    <h1>
 
-        Administrar Usuarios
+      Administrar Usuarios
 
-      </h1>
+    </h1>
 
       <ol class="breadcrumb">
 
-        <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+        <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
 
         <li class="active">Administrar Usuarios</li>
 
       </ol>
 
-    </section>
+  </section>
 
-    <!-- Main content -->
-    <section class="content">
+  <section class="content">
 
-      <!-- Default box -->
-      <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
+    <div class="box">
 
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                    title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
-          </div>
-        </div>
-        <div class="box-body">
-          Start creating your amazing application!
-        </div>
-        <!-- /.box-body -->
-        <div class="box-footer">
-          Footer
-        </div>
-        <!-- /.box-footer-->
+      <div class="box-header with-border">
+
+        <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarUsuario">
+
+          Agregar Usuario
+
+        </button>
+
       </div>
-      <!-- /.box -->
 
-    </section>
-    <!-- /.content -->
+      <div class="box-body">
+
+        <table class="table table-bordered table-striped dt-responsive tablas" width="100%">
+
+          <thead>
+
+            <tr>
+
+              <th style="width: 10px">#</th>
+              <th>Nombre</th>
+              <th>Usuario</th>
+              <th>Foto</th>
+              <th>Perfil</th>
+              <th>Estado</th>
+              <th>Último login</th>
+              <th>Acciones</th>
+
+            </tr>
+
+          </thead>
+
+          <tbody>
+
+            <tr>
+
+              <td>1</td>
+              <td>Usuario Administrador</td>
+              <td>admin</td>
+              <td><img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+              <td>Administrador</td>
+              <td><button class="btn btn-success btn-xs">Activado</button></td>
+              <td>2017-12-12 12:05:32</td>
+              <td>
+
+                <div class="btn-group">
+
+                  <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+
+                  <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+
+                </div>
+
+              </td>
+
+            </tr>
+
+          </tbody>
+
+        </table>
+
+      </div>
+
+    </div>
+
+  </section>
+
+</div>
+
+  <!--=============================================
+  =     MODAL AGREGAR USUARIO                    =
+  =============================================-->
+
+
+<div id="modalAgregarUsuario" class="modal fade" role="dialog">
+
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+
+      <form role="form" method="post" enctype="multipart/form-data">
+
+    <!--========================================
+    =     CABECERA DEL MODAL                    =
+    ============================================-->
+
+      <div class="modal-header" style="background: #3c8dbc; color: white">
+
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+        <h4 class="modal-title">Agregar usuario</h4>
+
+      </div>
+
+    <!--========================================
+    =     CUERPO DEL MODAL                     =
+    ============================================-->
+
+      <div class="modal-body">
+
+        <div class="box-body">
+
+          <!-- Entrada para el Nombre -->
+
+          <div class="form-group">
+
+            <div class="input-group">
+
+              <span class="input-group-addon"><i class="fa fa-user"></i></span>
+
+              <input type="text" class="form-control input-lg" name="nuevoNombre" placeholder="Ingresar nombre" required>
+
+            </div>
+
+          </div>
+
+          <!-- Entrada para el Usuario -->
+
+          <div class="form-group">
+
+            <div class="input-group">
+
+              <span class="input-group-addon"><i class="fa fa-key"></i></span>
+
+              <input type="text" class="form-control input-lg" name="nuevoUsuario" placeholder="Ingresar usuario" required>
+
+            </div>
+
+          </div>
+
+          <!-- Entrada para la contraseña -->
+
+          <div class="form-group">
+
+            <div class="input-group">
+
+              <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+
+              <input type="password" class="form-control input-lg" name="nuevoPassword" placeholder="Ingresar contraseña" required>
+
+            </div>
+
+          </div>
+
+          <!-- Entrada para seleccionar perfil -->
+
+          <div class="form-group">
+
+            <div class="input-group">
+
+              <span class="input-group-addon"><i class="fa fa-users"></i></span>
+
+              <select class="form-control input-lg" name="nuevoPerfil">
+
+                <option value="">Seleccionar perfil</option>
+
+                <option value="Administrador">Administrador</option>
+
+                <option value="Especial">Especial</option>
+
+                <option value="Vendedor">Vendedor</option>
+
+              </select>
+
+            </div>
+
+          </div>
+
+          <!-- Entrada para subir la foto -->
+
+          <div class="form-group">
+
+            <div class="panel">SUBIR FOTO</div>
+
+              <input type="file" id="nuevaFoto" name="nuevaFoto">
+
+              <p class="help-block">Peso máximo de la foto 2MB</p>
+
+              <img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail" width="100px">
+
+          </div>
+
+
+
+        </div>
+
+      </div>
+
+    <!--========================================
+    =     PIE PAGINA DEL MODAL                =
+    ============================================-->
+
+      <div class="modal-footer">
+
+        <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+
+        <button type="submit" class="btn btn-primary">Guardar usuario</button>
+
+      </div>
+
+      </form>
+
+    </div>
+
   </div>
-  <!-- /.content-wrapper -->
+
+</div>
