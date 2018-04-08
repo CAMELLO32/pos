cb79512e6f6ba9c715fbcbf60dc5919d336d8253
Jhon Alexander Miranda <camello32@gmail.com>
2018-04-07 23:14:25 -0500

    Inicio proyecto

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+
+	<strong>Copyright &copy; 2018 <a href="http://
+		wwww.tutorialesatualcance.com" target="_blanck">Tutoriales a tu Alcance</a>.
+	</strong>
+
+	Todos los derechos reservados.
+
+</footer>
